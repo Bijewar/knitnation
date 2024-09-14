3a999f7c9a678ea3224a9d715ae5cc2c8c7fe08c
Bijewar <bijewarmanas1@gmail.com>
2024-09-14 12:30:56 +0530

    Your commit message

--- app/login/page.js
+++ app/login/page.js
@@ -1,16 +1,18 @@
+// Updated LoginForm.js
+
 "use client";
 
 import React, { useState } from 'react';
 import Link from 'next/link';
-import Head from 'next/head'; // Import Head from next/head
+import Head from 'next/head';
 import { useRouter } from 'next/navigation';
 import { useDispatch } from 'react-redux';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { setUser, setError } from '../../redux/slices';
-import css from '../../style/login.css'; // Make sure this file is being included properly
+import css from '../../style/login.css'; // Ensure this file is being correctly imported
 import { auth } from '../../firebase';
 import withReduxProvider from '../hoc';
-import { toast, Toaster } from 'react-hot-toast';  // Added Toaster import here
+import { toast, Toaster } from 'react-hot-toast'; // Added Toaster import
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
@@ -41,67 +43,58 @@ const LoginForm = () => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Login</title> {/* Adding a title for the page */}
+        <title>Login</title>
       </Head>
-   
-        <Toaster toastOptions={{ duration: 4000 }} />
-        <form onSubmit={handleLogin}>
-         
-        </form>
-        <div className="custom-wrapper">
-            <div className="custom-image-wrapper">
-              <img src="/logo.png" alt="Logo" />
-            </div>
-            <div className="custom-form-wrapper">
-              <div className="custom-form-inner">
-             
-                <div className="custom-form-group">
-                  <input
-                    id="email"
-                    type="email"
-                    placeholder="Enter your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
-                  />
-                </div>
-    
-                <div className="custom-form-group">
-                  <input
-                    id="password"
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                
-               
-         
-                <button className='registerbtn' type="submit" disabled={loading}>
-                  {loading ? 'Logging in...' : 'Login'}
-                </button>
-                <p className='alrt'>
-                  <Link href="/register" legacyBehavior>
-                    <a>Forgot password?</a>
-                  </Link>
-                </p>
-<br />
-<Link href="/register" className="newacc">
-  Create a new Account
-</Link>
-            
-   
-                   
-              
 
-           
-              </div>
-          
-            </div>
-         
+      <Toaster toastOptions={{ duration: 4000 }} />
+
+      <form onSubmit={handleLogin}>
+        <div className="custom-wrapper">
+          <div className="custom-image-wrapper">
+            <img src="/logo.png" alt="Logo" />
           </div>
+          <div className="custom-form-wrapper">
+            <div className="custom-form-inner">
+              <div className="custom-form-group">
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="custom-form-group">
+                <input
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+
+              <button className="registerbtn" type="submit" disabled={loading}>
+                {loading ? 'Logging in...' : 'Login'}
+              </button>
+
+              <p className="alrt">
+                <Link href="/register">
+                  Forgot password?
+                </Link>
+              </p>
+
+              <br />
+              <Link href="/register" className="newacc">
+                Create a new Account
+              </Link>
+            </div>
+          </div>
+        </div>
+      </form>
     </>
   );
 };
